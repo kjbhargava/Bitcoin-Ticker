a5428feb71a23168d1f961ac96d7f68af76011a9
Kjbhargava <karanjbhargava@gmail.com>
2019-10-07 23:28:43 -0600

    api practice

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 //jshint esversion: 6
 const express = require("express");
 const bodyParser = require("body-parser");
+const request = require("request");
 
 const app = express();
 
@@ -8,6 +9,26 @@ app.use(bodyParser.urlencoded({extended:true}));
 
 app.get("/", function(req, res){
         res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/", function(req, res){
+    //console.log(req.body.crypto);
+    var url = "https://apiv2.bitcoinaverage.com/indices/global/ticker/";
+    var crypto = req.body.crypto;
+    var fiat = req.body.fiat;
+    url = url + crypto + fiat;
+
+    request(url, function(error, response, body){
+        var data = JSON.parse(body);
+        var price = data.last;
+
+        var currentDate = data.display_timestamp;
+
+        res.write("<p>"+"The current date is " + currentDate+"</p>");
+        res.write("<h1>"+"The price of "+crypto+" is "+price+fiat+"</h1>");
+
+    res.send();
+    });
 });
 
 app.listen(3000, function(){
